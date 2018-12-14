9c3154f2e11b4c48953c02167550cd0bbf0d668a
ClementLoubiere <clement.loubiere91@gmail.com>
2018-12-14 17:18:59 +0100

    Commit :  - Retirer une série de sa liste  - Eviter les doublons (on ne peut pas ajouter 2 fois la même série)  _ Commentaires diverses

--- src/Entity/Serie.php
+++ src/Entity/Serie.php
@@ -25,11 +25,6 @@
         private $idApi;
 
         /**
-         * @ORM\OneToMany(targetEntity="App\Entity\Episode", mappedBy="nb_episodes")
-         */
-        private $episodes;
-
-        /**
          * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="series")
          * @ORM\JoinColumn(nullable=false)
          */
@@ -72,37 +67,7 @@
             return $this;
         }
         
-
-        /**
-         * @return Collection|Episode[]
-         */
-        public function getEpisodes(): Collection
-        {
-            return $this->episodes;
-        }
-
-        public function addEpisode(Episode $episode): self
-        {
-            if (!$this->episodes->contains($episode)) {
-                $this->episodes[] = $episode;
-                $episode->setNbEpisodes($this);
-            }
-
-            return $this;
-        }
-
-        public function removeEpisode(Episode $episode): self
-        {
-            if ($this->episodes->contains($episode)) {
-                $this->episodes->removeElement($episode);
-                // set the owning side to null (unless already changed)
-                if ($episode->getNbEpisodes() === $this) {
-                    $episode->setNbEpisodes(null);
-                }
-            }
-
-            return $this;
-        }
+        
     
         public function __toString()
         {
