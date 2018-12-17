ae486d470a9755e03219b6621cbe3944106c610b
DavidNabet <david97.nabet@gmail.com>
2018-12-17 17:37:05 +0100

    adieu

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -6,9 +6,7 @@ use App\Entity\Episode;
 use App\Entity\Serie;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Common\Collections\Collection;
 
